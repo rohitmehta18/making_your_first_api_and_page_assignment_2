1890522ebb4439fbe68aab57c83a4bf9f1b46f24
rohitmehta18 <rmehta1836@gmail.com>
2025-02-04 12:06:27 +0530

    Completed solution for Virtual Assistant API and Status Code API

--- server.js
+++ server.js
@@ -50,6 +50,41 @@ List of Status Codes to Handle:
 200, 201, 204, 400, 401, 403, 404, 405, 429, 500, 502, 503, 504
 */
 
+const messages = {
+  200: "Request was successful.",
+  201: "A resource has been successfully created.",
+  204: "Request processed successfully, no content returned.",
+  400: "The request is invalid due to client-side errors.",
+  401: "Authentication is required to access the resource.",
+  403: "Server refuses to authorize the request.",
+  404: "The resource does not exist.",
+  405: "HTTP method not supported for this resource.",
+  429: "User has exceeded rate limits.",
+  500: "The server encountered an error.",
+  502: "The server received an invalid response from the upstream server.",
+  503: "Server temporarily overloaded or under maintenance.",
+  504: "The server did not receive a timely response from the upstream server."
+
+}
+
+app.get('/status-info', (req,res)=>{
+  const code = parseInt(req.query.code)
+
+ if(!code || !messages[code]){
+  return res.status(400).json({
+      error: 'bad request',
+      message : `Code does not exist`
+  })
+ }
+
+ res.json({
+  status: code,
+  message: messages[code]
+ })
+})
+
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Status Code API is running on http://localhost:${PORT}`);
